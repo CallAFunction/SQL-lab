3a9f0a313b1579b1213c4e92b3362c0c91ee0470
CallAFunction <139863134+CallAFunction@users.noreply.github.com>
2026-03-04 10:13:02 +0000

    Revise patient_registration table structure

--- create-patient-registration.sql
+++ create-patient-registration.sql
@@ -1,11 +1,9 @@
-CREATE TABLE patient_registration (  
-   patient_id INTEGER NOT NULL,
-   mission_id INTEGER NOT NULL,
-  -- role TEXT, --
-
-  PRIMARY KEY (astronaut_id, mission_id),
-  FOREIGN KEY (astronaut_id) REFERENCES astronauts(astronaut_id),
-  FOREIGN KEY (mission_id) REFERENCES missions(mission_id)
+CREATE TABLE patient_registration (
+    patient_id INTEGER NOT NULL,
+    professional_id INTEGER NOT NULL,
+    registration_date DATE,
+    
+    PRIMARY KEY (patient_id, professional_id),
+    FOREIGN KEY (patient_id) REFERENCES patient(patient_no),
+    FOREIGN KEY (professional_id) REFERENCES professional(professional_id)
 );
-
-

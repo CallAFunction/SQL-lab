b85c3cc835558f0b1b37919de38ba6dd1fd13b2b
CallAFunction <139863134+CallAFunction@users.noreply.github.com>
2026-03-04 10:07:36 +0000

    Add patient_id to patient_registration table

--- create-patient-registration.sql
+++ create-patient-registration.sql
@@ -1,10 +1,11 @@
-CREATE TABLE patient_registration (
-   INTEGER NOT NULL,
-  mission_id INTEGER NOT NULL,
+CREATE TABLE patient_registration (  
+   patient_id INTEGER NOT NULL,
+   mission_id INTEGER NOT NULL,
   -- role TEXT, --
 
   PRIMARY KEY (astronaut_id, mission_id),
   FOREIGN KEY (astronaut_id) REFERENCES astronauts(astronaut_id),
   FOREIGN KEY (mission_id) REFERENCES missions(mission_id)
 );
+
 
